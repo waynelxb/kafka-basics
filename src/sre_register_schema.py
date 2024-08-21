42e9b3b80523df7740f46c740f0f5ad27d6be250
waynelxb <waynelxb@gmail.com>
2024-08-20 20:34:11 -0500

    yes

--- src/sre_register_schema.py
+++ src/sre_register_schema.py
@@ -10,7 +10,7 @@ schema_registry_subject = f"user_schema_{compatibility_level}"
 # schema_registry_subject = "topic_user_schema_FORWARD-value"
 # delete_schema_registry_subject(schema_registry_url, schema_registry_subject)
 
-with open(f"avro_schema/{compatibility_level}/user_{compatibility_level}_v3.avsc", "r") as f:
+with open(f"sre_avro_schema/{compatibility_level}/user_{compatibility_level}_v3.avsc", "r") as f:
     schema_string = f.read()
 
 schema_id = register_schema(schema_registry_url, schema_registry_subject, schema_string, compatibility_level)
